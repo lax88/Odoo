01d98ed3865a432fc5dc2ed80289c963281ab19b
Laxmi Shrestha <102451303+lax88@users.noreply.github.com>
2024-07-09 00:00:01 +0545

    Add files via upload

---
+++ library_checkout/models/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Checkout(models.Model):
+        _name = "library.checkout"
+        
+        _description = "Checkout Request"
+
+        member_id = fields.Many2one("library.member",required=True,)
+        user_id = fields.Many2one("res.users", "Librarian", default=lambda s: s.env.user,)
+        request_date = fields.Date(default=lambda s: fields.Date.today(),)
+        line_ids = fields.One2many("library.checkout.line",
+                                    "checkout_id",
+                                    string="Borrowed Books",)
+
+
+class CheckoutLine(models.Model):
+        _name = "library.checkout.line"
+        _description = "Checkout Request Line"
+        _inherit = ["mail.thread", "mail.activity.mixin"]
+
+        checkout_id = fields.Many2one("library.checkout",required=True,)
+        book_id = fields.Many2one("library.book",required=True)
+        note = fields.Char("Notes")
+        
---
+++ ls_pos/static/src/js/mb_sample_button.js
@@ -0,0 +1,121 @@
+odoo.define("mb_pos.MohitSampleButton", function(require) {
+"use strict";
+
+    const PosComponent = require("point_of_sale.PosComponent");
+    const ProductScreen = require("point_of_sale.ProductScreen");
+    const Registeries = require("point_of_sale.Registries")
+    const { useListener } = require("@web/core/utils/hooks");
+    const  core  = require("web.core");
+    var _t = core._t;
+
+
+    class MohitSampleButton extends PosComponent {
+
+        setup(){
+            super.setup();
+            useListener("click",this.mb_sample_button_click );
+        }
+
+        async mb_sample_button_click(){
+
+            // Rendering Dynamic Data
+            var multi_lang = await this.rpc({
+                route:"/pos/rpc/example",
+                params:{},
+            });
+
+            console.log("language--->",multi_lang);
+
+            var multi_lang_list = [];
+
+            multi_lang.forEach(function(value){
+                multi_lang_list.push({"id":value.id,
+                "label":value.name,
+                 "item":value});
+            });
+            
+            console.log(multi_lang_list);
+
+            var {confirmed, payload: selectedOption} = await this.showPopup("SelectionPopup", {
+                 title:"Active Languages!",
+                 list: multi_lang_list
+            });
+            console.log(confirmed, selectedOption);
+            // var result = await this.rpc({
+            //     'model':"res.lang",
+            //     "method":"search_read",
+            //     "args":[[],['id','name','code']],
+            // })
+           
+
+            // RPC CALL
+            // var result = await this.rpc({
+            //     route: "/pos/rpc/example",
+            //     params: {}
+            // })
+            
+           
+
+            // result.forEach(function(value){
+            //     console.log("Record--->",value);
+            // })
+
+            // console.log(result);
+
+            //  Sound pop up
+            // this.showPopup("ErrorPopup", {
+            //     title: _t("Error message"),
+            //     body: this.env._t("The simple Error message"),
+            // });
+
+            // // Confirm popup
+            // var { confirmed }= await this.showPopup("ConfirmPopup",{
+            //     title: _t("Confirm Popup"),
+            //     body: _t("Are you sure to continue?"),
+            //     confirmText: _t("Yes"),
+            //     cancelText: _t("No")
+            // });
+            // // if (confirmed){
+            // //     console.log("clicked to Yes button")
+            // // }else{
+            // //     console.log("clicked to No button")
+            // // }
+            
+            // // offline error popup
+            // this.showPopup("OfflineErrorPopup",{
+            //     title: _t("Mohit Error"),
+            //     body: this.env._t("Hey this is the test popup screen okay don't take it serious!")
+            // });
+
+            // // Choice popup
+            // var { confirmed, payload: selectedOption } = await this.showPopup("SelectionPopup",{
+            //     title: ("Are you a good python developer."),
+            //     list:[{'id':0,'label': _t("Yes "),'item':"You pressed yes"},
+            //       {'id':1,'label': _t("No"),'item':"You pressed No"},
+            //       {'id':2,'label': _t("In confusion"),'item':"you pressed in confusion"}]
+            // });
+
+            // // console.log(confirmed);
+            // // console.log(selectedOption);
+            
+            // const info = await this.env.pos.getClosePosInfo();
+            // this.showPopup("ClosePosPopup",{
+            //     info: info,
+            //     KeepBehind: true
+            // })
+
+            console.log("Hello this is button click event pressed")
+            
+        }
+
+
+    }
+    MohitSampleButton.template = "MohitSampleButton";
+    ProductScreen.addControlButton({
+        component: MohitSampleButton,
+        position: ["before", "OrderlineCustomerNoteButton"],
+    });
+
+    Registeries.Component.add(MohitSampleButton);
+    return MohitSampleButton;
+});
